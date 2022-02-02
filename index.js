936cbbb4979b394516feea548724271f2cfee0ac
Utkarsh Yadav <72760223+iamutkarshyadav@users.noreply.github.com>
2022-02-02 16:48:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,36 +18,23 @@ client.prefix = PREFIX;
 client.queue = new Map();
 const cooldowns = new Collection();
 const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, `\\$&`);
-
-//this fires when the BOT STARTS DO NOT TOUCH
+// dont touch it!
 client.on(`ready`, () => {
-
-
-  ///////////////////////////////
-  ////////////IFCHEMPTY//////////
-  //remove everything in between those 2 big comments if you want to disable that the bot leaves when ch. or queue gets empty!
   setInterval(() => {
     let member;
     client.guilds.cache.forEach(async guild => {
       await delay(15);
       member = await client.guilds.cache.get(guild.id).members.cache.get(client.user.id)
-      //if not connected
       if (!member.voice.channel)
         return;
-      //if alone 
       if (member.voice.channel.members.size === 1) { return member.voice.channel.leave(); }
     });
-
-
-    client.user.setActivity(`${PREFIX}help | Made By Tech Boy Gaming`, {
-      type: "STREAMING",
-      url: "https://www.twitch.tv/nocopyrightsounds"
+ client.user.setActivity(`${PREFIX} UTKARSH, {
+      type: "Listening",
     });
 
 
   }, (5000));
-  ////////////////////////////////
-  ////////////////////////////////
   figlet.text(`${client.user.username} ready!`, function (err, data) {
     if (err) {
       console.log('Something went wrong');
@@ -64,8 +51,6 @@ client.on(`warn`, (info) => console.log(info));
 //DO NOT TOUCH
 client.on(`error`, console.error);
 //DO NOT TOUCH
-//FOLDERS:
-//Admin custommsg data FUN General Music NSFW others
 commandFiles = readdirSync(join(__dirname, `Music`)).filter((file) => file.endsWith(`.js`));
 for (const file of commandFiles) {
   const command = require(join(__dirname, `Music`, `${file}`));
@@ -79,33 +64,20 @@ for (const file of commandFiles) {
 //COMMANDS //DO NOT TOUCH
 client.on(`message`, async (message) => {
   if (message.author.bot) return;
-
-  //getting prefix 
   let prefix = await db.get(`prefix_${message.guild.id}`)
-  //if not prefix set it to standard prefix in the config.json file
   if (prefix === null) prefix = PREFIX;
-
-  //information message when the bot has been tagged
   if (message.content.includes(client.user.id)) {
     message.reply(new Discord.MessageEmbed().setColor("#F0EAD6").setAuthor(`${message.author.username}, My Prefix is ${prefix}, to get started; type ${prefix}help`, message.author.displayAvatarURL({ dynamic: true })));
   }
-  //An embed announcement for everyone but no one knows so fine ^w^
   if (message.content.startsWith(`${prefix}embed`)) {
-    //define saymsg
     const saymsg = message.content.slice(Number(prefix.length) + 5)
-    //define embed
     const embed = new Discord.MessageEmbed()
       .setColor("#F0EAD6")
       .setDescription(saymsg)
       .setFooter(client.user.username, client.user.displayAvatarURL())
-    //delete the Command
     message.delete({ timeout: 300 })
-    //send the Message
     message.channel.send(embed)
   }
-
-
-  //command Handler DO NOT TOUCH
   const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
   if (!prefixRegex.test(message.content)) return;
   const [, matchedPrefix] = message.content.match(prefixRegex);
