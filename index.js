cfb943b4f72bc05c60150e22d250a2fa82d29e44
Utkarsh Yadav <72760223+iamutkarshyadav@users.noreply.github.com>
2022-02-02 16:31:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const client = new Client({
   restTimeOffset: 0
 
 });
-client.login(TOKEN);
+client.login(process.env.TOKEN);
 client.commands = new Collection();
 client.prefix = PREFIX;
 client.queue = new Map();
